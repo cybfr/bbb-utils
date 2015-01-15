87d78eb5ae580925a701b2e0293c8b6e2e0e8fb7
root <root@niel-1.(none)>
2015-01-15 13:48:13 +0000

    suppressed local echo

--- cycleLeds.sh
+++ cycleLeds.sh
@@ -18,15 +18,15 @@ function reset_leds {
 }
 led_seq () {
 for state in $states ; do
-  echo -n "$state " 
   for i in `seq 0 3` ; do
     echo ${state:${i}:1} >${BASE}${i}/brightness
   done
-  sleep 0.2s
+  sleep 0.1s
 done
 }
 reset_leds
 for j in `seq 1 10`; do
+  sleep 1s
   led_seq
 done
 reset_leds
