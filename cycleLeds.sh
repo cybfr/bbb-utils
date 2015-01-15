2c594424c7414926a77a2e7bbfa0263eceabeb87
root <root@niel-1.miradou.com>
2015-01-15 03:14:45 +0000

    fixed bugs

--- cycleLeds.sh
+++ cycleLeds.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-set -x
-BASE=/sys/class/leds/beaglebone\:green\:usr
-states="1111 1011 1101 1110"
-init () {
+BASE="/sys/class/leds/beaglebone:green:usr"
+states="\
+1000 \
+1100 \
+1110 \
+1111 \
+0111 \
+0011 \
+0001 \
+0000 \
+"
+function reset_leds {
   for i in `seq 0 3` ; do
-   echo "none" >/${BASE}$i/trigger
-   echo 0  >/${BASE}$i/brightness
+   echo "none" >${BASE}$i/trigger
+   echo 0  >${BASE}$i/brightness
   done
 }
-seq () {
+led_seq () {
 for state in $states ; do
   echo -n "$state " 
   for i in `seq 0 3` ; do
     echo ${state:${i}:1} >${BASE}${i}/brightness
   done
-  sleep 0.5s
+  sleep 0.2s
 done
 }
-reset () {
-for i in `seq 0 3` ; do
- echo "none" >/${BASE}$i/trigger
- echo 0  >/${BASE}$i/brightness
+reset_leds
+for j in `seq 1 10`; do
+  led_seq
 done
-}
-init
-seq
-reset
+reset_leds
