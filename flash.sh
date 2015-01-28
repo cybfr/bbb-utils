c1ab048a1fcfb4a8b3482ceab0cdfe9f155e3dd8
root <root@graves.miradou.com>
2015-01-29 00:18:21 +0100

    use pv + ..

--- flash.sh
+++ flash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUM=1
+NUM=2
 ROOT_PART=$(mount | grep " / "|cut -d\  -f1)
 SRC=${ROOT_PART%*p?}
 DST=$(ls -1 /dev/mmcblk?|grep -v $SRC)
@@ -10,8 +10,14 @@ echo -n "Do it ? yes/NO " ; read rep
 
 if [[ "${rep}x" = "yesx" ]] 
 then
-dd if=/dev/zero of=$DST ds=1M count=10
-dd if=${SRC} of=${DST} bs=1M count=1900 &
+  echo "Flashing eMMC"
+  pv <${SRC} >${DST} -Ss 1832M
+fi
+
+if false
+then
+dd if=/dev/zero of=$DST bs=1M count=10
+dd if=${SRC} of=${DST} bs=1M count=1832 &
 PID=$!
 echo "/proc/$PID"
 
@@ -22,15 +28,16 @@ while [ -d /proc/${PID} ] ; do
 done
 fi
 sync
-blockdev --flushbufs ${destination}
+blockdev --flushbufs ${DST}
 
-umount /target
-umount ${DST}p1
-fsck ${DST}p1
-tune2fs -L "niel-${num}" $DST
+umount /target &>/dev/null
+umount ${DST}p1 &>/dev/null
+echo "Checking new filesystem"
+fsck -fTCp ${DST}p1 || exit 2
+tune2fs -L "niel-${NUM}" -U random /dev/mmcblk1p1 &>/dev/null
 mount ${DST}p1 /target
 sed -i "s/-x/-${NUM}/" /target/etc/hostname
-sed -i "s/NUM=$((NUM++))/NUM=$NUM/" /root/bbb-utils/flash.sh
+# sed -i "s/NUM=$((NUM++))/NUM=$NUM/" /root/bbb-utils/flash.sh
 rm -f /target/etc/ssh/ssh_host_{dsa,rsa,ecdsa}_key{.pub,}
 ssh-keygen -q -f /target/etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -q -f /target/etc/ssh/ssh_host_dsa_key -N '' -t dsa
