e5ff496fefddc158f20081524f25c5bb2431494d
root <root@graves.miradou.com>
2015-01-29 07:28:11 +0100

    Added build_mai

---
+++ build_mai.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get install \
+avahi-daemon bind9 btrfs-tools cloudprint-service \
+cron-apt curl dnsutils fail2ban hplip \
+locales logcheck lynx manpages nscd nslcd ntp ntpdate \
+openbsd-inetd openssl-blacklist screen slpd slptool \
+watchdog
+apt-get install  isc-dhcp-server
