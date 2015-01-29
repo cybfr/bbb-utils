8ba62570a6b0f093aca971123d62b97033f380a0
root <root@graves.miradou.com>
2015-01-29 12:49:19 +0100

    Added setup_mai.sh

---
+++ setup_mai.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt-get install avahi-daemon bind9 btrfs-tools cloudprint-service cron-apt curl dnsutils fail2ban hplip isc-dhcp-server locales logcheck lynx manpages nscd nslcd ntp ntpdate openbsd-inetd openssl-blacklist screen slpd slptool watchdog
